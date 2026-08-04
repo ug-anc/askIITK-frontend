4455c3b0bfffc26074a8a0772a452ffd48cdc80a
aaahanamehrotra <mehrotraaaahana@gmail.com>
2026-08-04 22:56:59 +0530

    v3 api route

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "https://ask-a-306291778036.asia-southeast1.run.app";
+export const API_BASE_URL = "https://backend-v3-306291778036.asia-southeast1.run.app";
+// export const API_BASE_URL = "http://localhost:8000";
 
 export const API_QUERY_URL = `${API_BASE_URL}/api/v1/query`;
 export const API_FEEDBACK_URL = `${API_BASE_URL}/api/v1/feedback`;
